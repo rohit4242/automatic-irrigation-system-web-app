37cfe15230fac74d6ff8e9084299a7085edbd3f7
rohit4242 <rohitluni4242@gmail.com>
2023-04-23 18:37:40 +0530

    init

--- Server/index.js
+++ Server/index.js
@@ -10,10 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    origin: "https://automatic-irrigation-system-website.vercel.app/",
   })
 );
 
